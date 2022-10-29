fb3f841f2734b3ef96507b763a9ba40f3a45ea16
vagechirkov <vagechirkov@gmail.com>
2022-10-29 17:01:00 +0200

    connect fastapi and ml-worker

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,13 +1,28 @@
+import asyncio
+
 from fastapi import FastAPI
 
 from models.MessageSchema import MessageSchema
 from rabbitmq.pika_client import PikaClient
 
 app = FastAPI()
-pika_client = PikaClient()
 
 
-@app.post('/send-message')
+def process_incoming_message(message):
+    print(message)
+
+
+pika_client = PikaClient(process_incoming_message)
+
+
+@app.on_event('startup')
+async def startup():
+    loop = asyncio.get_running_loop()
+    task = loop.create_task(pika_client.consume(loop))
+    await task
+
+
+@app.post('/send-task')
 async def send_message(payload: MessageSchema):
     pika_client.send_message({"message": payload.message})
     return {"status": "ok"}
--- fastapi/app/rabbitmq/pika_client.py
+++ fastapi/app/rabbitmq/pika_client.py
@@ -2,12 +2,14 @@ import json
 import uuid
 
 import pika as pika
+from aio_pika import connect_robust
 from envparse import env
 
 
 class PikaClient:
-    def __init__(self):
-        self.publish_queue_name = env('PUBLISH_QUEUE', 'noname_publish_queue')
+    def __init__(self, process_callable):
+        self.publish_queue_name = env(
+            'PUBLISH_TASK_QUEUE', 'noname_publish_task_queue')
         self.connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=env('RABBIT_HOST', '127.0.0.1'))
         )
@@ -16,6 +18,24 @@ class PikaClient:
             queue=self.publish_queue_name)
         self.callback_queue = self.publish_queue.method.queue
         self.response = None
+        self.process_callable = process_callable
+
+    async def consume(self, loop):
+        """Setup message listener with the current running loop"""
+        connection = await connect_robust(
+            host=env('RABBIT_HOST', '127.0.0.1'), port=5672, loop=loop)
+        channel = await connection.channel()
+        queue = await channel.declare_queue(
+            env('CONSUME_RESULT_QUEUE', 'noname_consume_result_queue'))
+        await queue.consume(self.process_incoming_message, no_ack=False)
+        return connection
+
+    async def process_incoming_message(self, message):
+        """Processing incoming message from RabbitMQ"""
+        await message.ack()
+        body = message.body
+        if body:
+            self.process_callable(json.loads(body))
 
     def send_message(self, message: dict):
         """Method to publish message to RabbitMQ"""
