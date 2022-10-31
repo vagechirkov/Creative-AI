8fe2914e03c0c3dd01704e6b30c73a89992a945d
vagechirkov <vagechirkov@gmail.com>
2022-10-31 20:58:54 +0100

    prevent double submissions

--- backend/app/static/main.js
+++ backend/app/static/main.js
@@ -1,24 +1,30 @@
 function handleClick() {
-  const text = document.getElementById('promptText').value || 'default';
+    const text = document.getElementById('promptText').value || 'default';
+    const bnt = document.getElementById('promptButton');
 
-  fetch(`/generate/${text}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-  })
-  .then(response => response.json())
-  .then(res => {
-    console.log(res)
-    const date = new Date();
-    const time = date.toLocaleTimeString();
-    const html = `
+    // Disable the button to prevent multiple clicks
+    bnt.disabled = true;
+
+
+    fetch(`/generate/${text}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+        .then(response => response.json())
+        .then(res => {
+            console.log(res)
+            const date = new Date();
+            const time = date.toLocaleTimeString();
+            const html = `
       <tr>
         <td>${res.task_id}</td>
         <td>${time}</td>
         <td>${res.task_result}</td>
       </tr>`;
-    const newRow = document.getElementById('tasks').insertRow(0);
-    newRow.innerHTML = html;
-  })
+            const newRow = document.getElementById('tasks').insertRow(0);
+            newRow.innerHTML = html;
+            bnt.disabled = false;
+        })
 }
