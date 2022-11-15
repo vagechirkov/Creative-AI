0ffb570a4c72d93795672ad88f3bb2b863716b08
vagechirkov <vagechirkov@gmail.com>
2022-11-15 11:55:09 +0100

    update frontend

--- backend/app/static/main.js
+++ backend/app/static/main.js
@@ -3,13 +3,9 @@ const user_id = Math.floor(Math.random() * 10000);
 function handleClick() {
     const text = document.getElementById('promptText').value || 'default';
     const bnt = document.getElementById('promptButton');
-    
-    const date = new Date();
-    const time_start = date.toLocaleTimeString();
 
     // Disable the button to prevent multiple clicks
     bnt.disabled = true;
-
 
     fetch(`/generate/${user_id}/${text}`, {
         method: 'GET',
@@ -19,19 +15,10 @@ function handleClick() {
     })
         .then(response => response.json())
         .then(res => {
-            console.log(res)
-            const date = new Date();
-            const time_end = date.toLocaleTimeString();
-            const html = `
-      <tr>
-        <td>${user_id}</td>
-        <td>${res.task_id}</td>
-        <td>${time_start}</td>
-        <td>${time_end}</td>
-        <td>${res.task_result}</td>
-      </tr>`;
-            const newRow = document.getElementById('tasks').insertRow(0);
-            newRow.innerHTML = html;
+            console.log(res);
+            // add new image url
+            const img = document.getElementById('resultImage');
+            img.src = res.task_result;
             bnt.disabled = false;
         })
 }
