73ab67dd57d3b70de91e656ac2d5d1e4591a0080
vagechirkov <vagechirkov@gmail.com>
2022-11-16 10:21:16 +0100

    update generate route

--- backend/app/routes/generate.py
+++ backend/app/routes/generate.py
@@ -3,42 +3,32 @@ import logging
 
 from celery.result import AsyncResult
 from fastapi import APIRouter
-from starlette.responses import JSONResponse
 
 import config
 from celery_worker.tasks import generate_image
+from models.image_generation import GenerateImagesRequest, \
+    GeneratedImagesResponse
 
 logger = logging.getLogger(__name__)
 
 generate_router = APIRouter()
 
 
-@generate_router.get("/{user_id}/{generate_query}")
-async def get_image(user_id: str, generate_query: str):
-    task = generate_image.apply_async((generate_query, 5),
-                                      queue=config.TASK_QUEUE)
-    logger.info(f"Task ID: {task.id}")
+@generate_router.post("/{user_id}/")
+async def post_image_query(
+        user_id: str,
+        generate_params: GenerateImagesRequest) -> GeneratedImagesResponse:
+    task = generate_image.apply_async(
+        args=[generate_params.prompt],
+        kwargs=generate_params.dict(),
+        queue=config.TASK_QUEUE)
+    logger.info(f"FastAPI: Task {task.id} has been created")
     result = await get_task_result(task.id)
-    return JSONResponse({"task_id": task.id, "task_result": result})
-
-
-@generate_router.post("/{generate_query}")
-async def post_image_query(generate_query: str):
-    task = generate_image.apply_async((generate_query,),
-                                      queue=config.TASK_QUEUE)
-    logger.info(f"Task {task.id} has been created")
-    return JSONResponse({"task_id": task.id})
-
-
-@generate_router.get("/{task_id}")
-def get_status(task_id):
-    task_result = AsyncResult(task_id)
-    result = {
-        "task_id": task_id,
-        "task_status": task_result.status,
-        "task_result": task_result.result
-    }
-    return JSONResponse(result)
+    logger.info(f"FastAPI: Task {task.id} has been completed")
+    return GeneratedImagesResponse(
+        requestId=generate_params.requestId,
+        images=result,
+        celery_task_id=task.id)
 
 
 async def get_task_result(task_id):
