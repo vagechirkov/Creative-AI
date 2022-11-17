a9a09d5472b3fc6262a6599255aad31cb9a93ad8
vagechirkov <vagechirkov@gmail.com>
2022-11-17 14:01:01 +0100

    remove simple frontend from the fastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,6 @@
 import logging.config
 
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 from logging_config import LOGGING
@@ -16,15 +14,6 @@ app = FastAPI()
 # metrics on the /metrics endpoint for prometheus
 app.add_middleware(PrometheusMiddleware)
 app.add_route("/metrics", handle_metrics)
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
-
-@app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("home.html", context={"request": request})
-
 
 app.include_router(generate_router, prefix="/generate", tags=["Generate"])
 
